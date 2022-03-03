6742753496d75c5c7399ecbd90d8bf4bda1c192e
Girlian Santos <girliansantos@si.fiponline.edu.br>
2022-03-02 21:43:11 -0300

    criado componente agenda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventoComponent } from './components/evento/evento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventoComponent
   ],
   imports: [
     BrowserModule,
